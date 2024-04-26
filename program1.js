068c4b2bfbd228223c90e4a725de55f98e1a31c9
Pranay yadav <144846552+pranayy08@users.noreply.github.com>
2024-04-26 13:09:06 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,9 +1,21 @@
 function smallestMissingPositiveInteger(nums) {
+  // Remove non-positive integers and duplicates
+  nums = nums.filter((num, index) => num > 0 && nums.indexOf(num) === index);
 
+  // Sort the array
+  nums.sort((a, b) => a - b);
 
-    // Implement the function smallest_missing_positive_integer
-  
+  let missing_num = 1;
+  for (const num of nums) {
+      if (num === missing_num) {
+          missing_num++;
+      } else {
+          return missing_num;
+      }
   }
-  
-  module.exports = smallestMissingPositiveInteger;
+  return missing_num;
+}
 
+module.exports = smallestMissingPositiveInteger;
+
+  
