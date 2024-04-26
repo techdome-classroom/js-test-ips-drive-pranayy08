c1c2a3c34167f163fe55f9378387f9bd16b45e80
Pranay yadav <144846552+pranayy08@users.noreply.github.com>
2024-04-26 13:09:34 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -2,6 +2,18 @@ function longestSubstring(s) {
 
 
     // Implementation of longestSubstring function
+    let longest=0;
+    let seen={};
+    let start=0;
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        if (seen[char]) {
+            start = Math.max(start,seen[char])
+        }
+        longest=Math.max(longest,i-start+1)
+        seen[char]=i+1;
+    }
+    return longest;
 }
 
 module.exports = { longestSubstring };
